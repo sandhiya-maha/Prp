67e6e85992ff50114cfae6df47775a346d795933
sandhiya-maha <66624107+sandhiya-maha@users.noreply.github.com>
2020-06-14 19:56:11 +0530

    Add files via upload

---
+++ Basics/Sample1.java
@@ -0,0 +1,8 @@
+public class Sample1{
+public static void main(String args[])
+{
+String company=args[0];
+String city=args[1];
+System.out.println(company+" Technologies "+city);
+}
+}
---
+++ Basics/Sample3.java
@@ -0,0 +1,9 @@
+public class Sample3
+{
+public static void main(String args[])
+	{
+		int number1=Integer.parseInt(args[0]);
+		int number2=Integer.parseInt(args[1]);
+		System.out.println("The sum of "+number1+" and "+number2+" is "+(number1+number2));
+	}
+}
